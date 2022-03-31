12693810388985ca3c3db8dc0cb785c4ae391d9b
henriqueberaldo11 <henriqueberaldo11@gmail.com>
2022-03-31 11:50:28 -0300

    feat: implementação Car

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/createSpecificationController";
-
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
